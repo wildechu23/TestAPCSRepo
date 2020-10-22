3e9331753c26c21fce90bb3d33080f8ab9037348
Wilde Chu <wildechu23@gmail.com>
2020-10-21 21:11:55 -0400

    Added all array functions, tested and works

--- Demo.java
+++ Demo.java
@@ -5,6 +5,10 @@ public class Demo {
     } else {
       printLoop(5);
     }
+
+    System.out.println(arrDeepToString(create2DArray(5, 4, 4)).replace("}, ","},\\n"));
+    System.out.println(arrDeepToString(create2DArrayRandomized(6, 4, 4)).replace("}, ","},\n "));
+    System.out.println(arrDeepToString(create2DArrayRandomized(4, 10, 100)).replace("}, ", "},\n "));
   }
 
   public static void printLoop(int n) {
@@ -14,5 +18,51 @@ public class Demo {
       }
       System.out.println();
     }
+  }
+
+  // print an array as a string in the format {0, 1, 2}
+  public static String arrToString(int[] arr) {
+    String string = "";
+    for (int i = 0; i < arr.length; i++) {
+      string += arr[i];
+      if (i != arr.length - 1) {
+        string += ", ";
+      }
+    }
+    return "{" + string + "}";
+  }
+
+  // print a 2d array as a string in the format {{0}, {1, 2} {1, 2, 12}}
+  public static String arrDeepToString(int[][] arr) {
+    String string = "";
+    for (int i = 0; i < arr.length; i++) {
+      string += arrToString(arr[i]);
+      if (i != arr.length - 1) {
+        string += ", ";
+      }
+    }
+    return "{" + string + "}";
+  }
+
+  public static int[][] create2DArray(int rows, int cols, int maxValue) {
+    int[][] array = new int[rows][cols];
+    for(int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        array[i][j] = (int)(Math.random() * (maxValue + 1));
+      }
+    }
+    return array;
+  }
+  
+  public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue) {
+    int[][] array = new int[rows][];
+    for (int i = 0; i < rows; i++) {
+      int maxCols = (int)(Math.random() * (cols + 1));
+      array[i] = new int[maxCols];
+      for (int j = 0; j < maxCols; j++) {
+        array[i][j] = (int) (Math.random() * (maxValue + 1));
+      }
+    }
+    return array;
   }
 }
